f83acf757d17b353b2be0fad415fb06049c2b3f7
Андрей <79538233369@ya.ru>
2022-04-24 19:42:27 +0500

    cycles video

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,21 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Cycle iteration " + i);
+        }
+        for (int i = 2020; i <= 2070; i = i + 4) {
+            System.out.println("Leap year " + i);
+        }
+        int salary = 65535;
+        int total = 0;
+        int i = 0;
+        for (; total <= 1_000_000; i++) {
+            total = total + total / 100;
+            total = total + salary;
+            System.out.println(total);
+     //       System.out.println("Month " + i + ". Total " + total);
+        }
+    //      System.out.println("It will take " + i + " months!");
     }
 }
