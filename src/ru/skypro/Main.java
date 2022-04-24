e99c9ec19b631175ad2b56e45354e78b1436005f
Андрей <79538233369@ya.ru>
2022-04-24 22:15:27 +0500

    5.3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,9 +3,7 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        int Friday = 1;
-        int day = 1;
-        for (; Friday < 31; Friday = Friday+7) {
-            System.out.println("today is the " + Friday + "th of April 2022. It's Friday today. " + "Please, don't forget to prepare the report!");
+        for (int cometYear = 1896, currentYear = 2022, hundredYearsAfter = 2122, cometPeriod = 79; cometYear < hundredYearsAfter; cometYear = cometYear + cometPeriod) {
+            System.out.println(cometYear);
         }
 }}
